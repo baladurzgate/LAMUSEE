9cc9f0590baec703e7d435b005c343a5eb90443b
Alexandre <bighal.cormier@gmail.com>
2015-06-10 11:31:18 +0200

    conditions of content on displaying text and details link

--- lamusee/template_illustration.php
+++ lamusee/template_illustration.php
@@ -2,7 +2,7 @@
 	
 	<figure id="illustration">
 		<div class = "imgborder">
-			<img src="<?php echo $image['url']; ?>"  border="0" usemap="#Map<?php $post->ID; ?>"/>
+			<img id = "tableau" src="<?php echo $image['url']; ?>"  border="0" usemap="#Map<?php $post->ID; ?>"/>
 			<map name="Map<?php echo $linked_object->ID; ?>" id="Map<?php $post->ID; ?>">
 					<?php echo $modifed_areas; ?>
 			</map>
@@ -14,11 +14,20 @@
 		<div id="history">...</div>
 	</div>
 	
-	<div class="fils-right">
-		<div class="slide-right"><a href="<?php echo $text_link; ?>">â–²<br>texte</a></div>
-	</div>
+	<?php if(has_text()):?>
 	
-	<div class="fils-left">
-		<div class="slide-left"><a href="<?php echo $details_link; ?>">â–²<br>detail</a></div>
-	</div>
+		<div class="fils-right">
+			<div class="slide-right"><a href="<?php echo $text_link; ?>">â–²<br>texte</a></div>
+		</div>
+	
+	<?php endif;?>
+	
+	<?php if(has_details()):?>
+	
+		<div class="fils-left">
+			<div class="slide-left"><a href="<?php echo $details_link; ?>">â–²<br>detail</a></div>
+		</div>
+	
+	<?php endif;?>
+	
 	
