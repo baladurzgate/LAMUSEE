0b535328e24994c30d147855350feeceebea4bf2
Alexandre <bighal.cormier@gmail.com>
2015-08-03 17:14:38 +0200

    Delete template_carte.php

--- template_carte.php
+++
@@ -1,8 +0,0 @@
-<?php /* Template Name: Carte Info */ get_header(); ?>
-<div id="carte">
-		<?php if($artiste) echo $artiste; ?>
-		<i> <?php if($titre_du_tableau) echo $titre_du_tableau; ?></i>
-		<?php if($date) echo ', '.$date; ?>
-		<br>
-		<?php if($dimensions) echo $dimensions; ?><?php if($technique) echo ', '.$technique; ?><?php if($lieu_de_conservation) echo ', '.$lieu_de_conservation; ?>
-	</div>
